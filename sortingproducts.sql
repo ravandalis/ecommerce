2f834be2b47c8160375771f290f55c1411be6489
Rod Angelo V. Andalis <125167729+ravandalis@users.noreply.github.com>
2023-03-18 23:25:14 +0800

    Create sortingproducts.sql

---
+++ sortingproducts.sql
@@ -0,0 +1,11 @@
+To sort the products by price, from lowest to highest:
+SELECT * FROM products ORDER BY price DESC;
+
+To sort the products by price, from highest to lowest:
+SELECT * FROM products ORDER BY price DESC;
+
+To sort the products by popularity, from most popular to least popular:
+SELECT * FROM products ORDER BY popularity DESC;
+
+To sort the products by fragrance type, alphabetically:
+SELECT * FROM products ORDER BY fragrance_type ASC;
