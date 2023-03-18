9192b6568f1f18b3d5dc465617eacf31ecf39ed4
Rod Angelo V. Andalis <125167729+ravandalis@users.noreply.github.com>
2023-03-18 23:25:56 +0800

    Create insert.sql

---
+++ insert.sql
@@ -0,0 +1,4 @@
+Inserting new perfume products: Whenever a new perfume product is added to your website's inventory, its details such as name, description, price, brand, and quantity must be inserted into the perfume table of the database.
+
+INSERT INTO perfume (name, description, price, brand, quantity)
+VALUES ('Chanel No.5', 'A classic floral fragrance for women', 100.00, 'Chanel', 50);
